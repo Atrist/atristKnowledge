cefe958f8367ede7ae550795e2487d4c62187c05
atrist <1640819339@qq.com>
2019-12-16 14:09:59 +0800

    vue02

--- 03project-based-learning/Vue/vue-firebase-project/src/main.js
+++ 03project-based-learning/Vue/vue-firebase-project/src/main.js
@@ -4,16 +4,7 @@ import router from './router'
 import firebase from 'firebase';
 
 Vue.config.productionTip = false
-const firebaseConfig = {
-  apiKey: "AIzaSyAKZc4Zc6QjNSd8wnZnGeCS-SBkXPj0UOM",
-  authDomain: "atristvuefirebase.firebaseapp.com",
-  databaseURL: "https://atristvuefirebase.firebaseio.com",
-  projectId: "atristvuefirebase",
-  storageBucket: "atristvuefirebase.appspot.com",
-  messagingSenderId: "996783481619",
-  appId: "1:996783481619:web:414f5b30d5ab602314b0e6",
-  measurementId: "G-TD8545K8DD"
-};
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
