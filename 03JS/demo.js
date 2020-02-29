0343e20151eb62082cab78cf8661f8f67128a140
atrist <atristmu@outlook.com>
2020-02-29 11:50:45 +0800

    js02基本类型

--- 03JS/demo.js
+++ 03JS/demo.js
@@ -1,7 +1,18 @@
-
-const hd = ["hdcms", "houdunren"];
-
-for (const [key, value] of hd.entries()) {
-  console.log(key, value); //这样就可以遍历了
+function dateFormat(date, format = "YYYY-MM-DD HH:mm:ss") {
+  const config = {
+    YYYY: date.getFullYear(),
+    MM: date.getMonth() + 1,
+    DD: date.getDate(),
+    HH: date.getHours(),
+    mm: date.getMinutes(),
+    ss: date.getSeconds()
+  };
+  for (const key in config) {
+    format = format.replace(key, config[key]);
+  }
+  return format;
 }
+console.log(dateFormat(new Date(), "YYYY年MM月DD日HH时mm分ss秒"));
+let js = new Date();
+console.log(js.toJSON());
 
