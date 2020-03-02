79c61a74b3e1b21c7a7899da99239fe79043e86b
atrist <atristmu@outlook.com>
2020-03-02 13:51:27 +0800

    js Symbol set map

--- 03JS/demo.js
+++ 03JS/demo.js
@@ -1,18 +1,10 @@
-function dateFormat(date, format = "YYYY-MM-DD HH:mm:ss") {
-  const config = {
-    YYYY: date.getFullYear(),
-    MM: date.getMonth() + 1,
-    DD: date.getDate(),
-    HH: date.getHours(),
-    mm: date.getMinutes(),
-    ss: date.getSeconds()
-  };
-  for (const key in config) {
-    format = format.replace(key, config[key]);
-  }
-  return format;
-}
-console.log(dateFormat(new Date(), "YYYY年MM月DD日HH时mm分ss秒"));
-let js = new Date();
-console.log(js.toJSON());
+let arr = [7, 6, 2, 8, 2, 6];
+let set = new Set(arr);
+//使用forEach遍历
+set.forEach((item,key) => console.log(item,key));
+
+
+
+
+
 
