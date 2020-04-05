8744cd1bced9da5edf56bae436919c3be7143dcd
atrist <atristmu@outlook.com>
2020-04-05 15:40:13 +0800

    JS30-19

---
+++ 08projectForLearning/js30/19 - Webcam Fun/scripts.js
@@ -0,0 +1,97 @@
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
+
+function getVideo() {
+    navigator.mediaDevices.getUserMedia({
+            video: true
+        })
+        .then(function (stream) {
+            //  DEPRECIATION : 
+            //       The following has been depreceated by major browsers as of Chrome and Firefox.
+            //       video.src = window.URL.createObjectURL(localMediaStream);
+            //       Please refer to these:
+            //       Depreceated  - https://developer.mozilla.org/en-US/docs/Web/API/URL/createObjectURL
+            //       Newer Syntax - https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/srcObject
+
+            video.srcObject = stream;
+            video.play();
+        })
+        .catch(function (err) {
+            console.log("An error occured! " + err);
+        });
+}
+
+function paintToCanvas() {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    canvas.width = width;
+    canvas.height = height;
+    return setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height);
+        let pixels = ctx.getImageData(0, 0, width, height);
+        pixels = rgbSplit(pixels);
+        ctx.putImageData(pixels, 0, 0);
+    }, 16);
+}
+
+function takePhoto() {
+    snap.currentTime = 0;
+    snap.play();
+
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'handsome');;
+    link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
+    strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i + 0] = pixels.data[i + 0] + 200; //红
+        pixels.data[i + 1] = pixels.data[i + 1] - 50; //绿
+        pixels.data[i + 2] = pixels.data[i + 2] * 0.5; //蓝
+    }
+    return pixels;
+}
+
+function rgbSplit(pixels) {
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i - 150] = pixels.data[i + 0]; //红
+        pixels.data[i + 500] = pixels.data[i + 1]; //绿
+        pixels.data[i - 550] = pixels.data[i + 2]; //蓝
+    }
+    return pixels;
+}
+
+function greenScreen(pixels) {
+    const levels = {};
+    document.querySelectorAll('.rgb input').forEach((input) => {
+        levels[input.name] = input.value;
+    });
+
+    for (i = 0; i < pixels.data.length; i = i + 4) {
+        red = pixels.data[i + 0];
+        green = pixels.data[i + 1];
+        blue = pixels.data[i + 2];
+        alpha = pixels.data[i + 3];
+
+        if (red >= levels.rmin &&
+            green >= levels.gmin &&
+            blue >= levels.bmin &&
+            red >= levels.rmax &&
+            green <= levels.gmax &&
+            blue <= levels.bmax) {
+            pixels.data[i + 3] = 0;
+        }
+    }
+
+    return pixels;
+}
+
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
